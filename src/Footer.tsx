baaf75f8618c9a9155011320c636ab1015a463fa
rjarivi <124758732+rjarivi@users.noreply.github.com>
2024-12-25 10:29:36 +0530

    Create Footer.tsx

---
+++ src/Footer.tsx
@@ -0,0 +1,12 @@
+// src/Footer.tsx
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer text-center py-4 bg-[#16171D] border-t border-[#262933]">
+      <p>made with love by <a href="https://x.com/rjarivi" target="_blank" rel="noopener noreferrer" className="text-[#4fffbc]">@rjarivi</a></p>
+    </footer>
+  );
+};
+
+export default Footer;
